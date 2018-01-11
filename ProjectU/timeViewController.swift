f4ceac63e99d6984ce9546bfe3dbb16360719106
osyou-create <hanzo.rl0422@icloud.com>
2018-01-12 01:51:57 +0900

    時間設定UIのみ

---
+++ ProjectU/timeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  timeViewController.swift
+//  ProjectU
+//
+//  Created by Naoya on 2018/01/12.
+//  Copyright © 2018年 osyou. All rights reserved.
+//
+
+import UIKit
+
+class timeViewController: UIViewController {
+
+    @IBOutlet weak var uptime_label: UILabel!
+    @IBOutlet weak var uptime: UIDatePicker!
+    @IBOutlet weak var uptime_config: UIButton!
+    @IBOutlet weak var sunup: UISwitch!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
