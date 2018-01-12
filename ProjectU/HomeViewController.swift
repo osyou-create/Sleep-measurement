cce717a7181e3aac6f23338e6387b3e0b7b6d048
K-Nino <nino-1115@outlook.jp>
2018-01-12 17:57:33 +0900

    201801121757

--- ProjectU/HomeViewController.swift
+++ ProjectU/HomeViewController.swift
@@ -19,15 +19,15 @@ class HomeViewController: UIViewController {
         homeWebView.loadRequest(request)
     }
     
-    func userAgent(){
-        let newUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_1 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+    func updateUserAgent(){
+        let newUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
         let dic = ["UserAgent" : newUserAgent]
         UserDefaults.standard.register(defaults: dic)
     }
     
     
     override func viewDidLoad() {
-        self.userAgent()
+        self.updateUserAgent()
         super.viewDidLoad()
         loadURL()
     }
--- ProjectU/timeViewController.swift
+++ ProjectU/timeViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class timeViewController: UIViewController {
 
+    var temp:String!
+    var temp2:String!
+    var temp_second:Int!
+
     @IBOutlet weak var uptime_label: UILabel!
-    @IBOutlet weak var uptime: UIDatePicker!
-    @IBOutlet weak var uptime_config: UIButton!
+    @IBOutlet weak var uptime_label2: UILabel!
     @IBOutlet weak var sunup: UISwitch!
+    @IBAction func uptime(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        let formatter2 = DateFormatter()
+        formatter.dateFormat = "HH"
+        formatter2.dateFormat = "mm"
+        temp = formatter.string(from: sender.date)
+        temp2 = formatter2.string(from: sender.date)
+        
+    }
+    @IBAction func uptime_push(_ sender: UIButton) {
+        /*
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        let now = Date()
+        */
+        uptime_label.text = temp
+        temp_second = Int(temp)!*3600 + Int(temp2)!*60
+        print(String(temp_second))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(temp_second)) {
+            print("aaaaa")
+        }
+
+    }
     
     
-    
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
